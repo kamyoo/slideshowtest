a119ccb602a523b2e0ac319155030098584d7f26
Kamy A <141620918+kamyoo@users.noreply.github.com>
2024-09-05 10:37:56 +0200

    Add files via upload

---
+++ scripts/slider.js
@@ -0,0 +1,15 @@
+let opts = {
+    //auto-advancing slides? accepts boolean (true/false) or object
+    auto : { 
+        // speed to advance slides at. accepts number of milliseconds
+        speed : 2500, 
+        // pause advancing on mouseover? accepts boolean
+        pauseOnHover : true 
+    },
+    // show fullscreen toggle? accepts boolean
+    fullScreen : true, 
+    // support swiping on touch devices? accepts boolean, requires hammer.js
+    swipe : true 
+};
+
+makeBSS('.slideshow-class-goes-here', opts);
